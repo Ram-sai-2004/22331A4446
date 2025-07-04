fe778d3233686aee3acf5ec91c2b94584e875a2b
Ram Sai Sripathi <sripathiramsai2004@gmail.com>
2025-07-04 12:55:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import logo from './logo.svg';
+import react from "react";
 import './App.css';
+import axios from "axios";
+
+const BASE_API_URL ="https://20.244.56.144/evaluation-service/auth"
+
+const api = axios.create() =>{
+  const baseURL = BASE_API_URL,
+  timeout:10000,
+  headers:
+  "contentType";"application/json"
+}
+
+export getRequest(endpoint,params{} =>{
+  try{
+    console.postRequest(endpoint,params{});
+    Response.add(data);
+  }
+  catch(error){
+    console.error(POST ${endpoint});
+    throw error;
+  }
+};
+
+export postRequest(endpoint,data => {
+  try{
+    console.postRequest(endpoint,data);
+    Response.add(data);
+  }
+  catch(error){
+    console.error(POST ${endpoint});
+    throw error;
+  }
+};
+)
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1 className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
